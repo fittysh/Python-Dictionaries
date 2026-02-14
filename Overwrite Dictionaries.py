32bae55db530acd82f65acb138f7f4404326d148
Fitri Shamsudin <fittygamingz@gmail.com>
2026-02-14 16:02:48 +0800

    Add files via upload

---
+++ Overwrite Dictionaries.py
@@ -0,0 +1,14 @@
+# Overwrite Dictionaries
+# -- For breakfast menu
+menu = {"oatmeal": 3, "avocado toast": 6, "carrot juice": 5, "blueberry muffin": 2}
+print(menu)
+menu["oatmeal"] = 19
+print(menu)
+
+# For Oscar Winners
+
+oscar_winners = {"Best Picture": "La La Land", "Best Actor": "Casey Affleck", "Best Actress": "Emma Stone", "Animated Feature": "Zootopia"}
+oscar_winners["Supporting Actress"] = "Viola Davis"
+print(oscar_winners)
+oscar_winners["Best Picture"] = "Moonlight"
+print(oscar_winners)
